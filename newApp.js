9194a58a12d752442022d83e2f7baff8ab08d0b5
Truong Nguyen <nhattruong159@gmail.com>
2018-06-01 11:43:24 +0700

    + modify socket + database with body-parser

--- newApp.js
+++ newApp.js
@@ -2,6 +2,30 @@
 var {config} = require('./config');
 var express = require('express');
 
+var bodyParser = require('body-parser');
+
+//Start Redis
+var redis = require("redis");
+var client = redis.createClient();
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+var userCount = 0;
+client.get("userCount", function(err, obj)
+{
+    if(obj != null)
+        userCount = parseInt(obj);
+});
+//end Redis
+
+//start body parser
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+//end body parser
+
 //config Port
 var conUserHttp = express();
 conUserHttp.listen(config.user.http.port, () =>{
@@ -9,7 +33,7 @@ conUserHttp.listen(config.user.http.port, () =>{
 });
 
 var conUserWebSocket = require('ws');
-var wss = new conUserWebSocket.Server({ port: 8001});
+var wss = new conUserWebSocket.Server({ port: config.user.webSocket.port});
 wss.on('connection', function connection(ws) {
     console.log('client connect');
 });
@@ -21,9 +45,34 @@ conAdmin.listen(config.admin.port,()=>{
 
 /*Update Content*/
 //Add User
+
+// parse various different custom JSON types as JSON
+conUserHttp.use(jsonParser);
 conUserHttp.put('/user', (req, res) =>
 {
-    res.send('Create user Success');
+    
+    var username = req.body.username;
+    var score = req.body.score;
+    
+    console.log("userCount= " + userCount);
+
+    client.get("userCount", function(err, obj)
+    {
+        client.hset("userId:" + userCount,"userName", username, redis.print);
+        client.hset("userId:" + userCount,"score", score, redis.print);
+
+        //client.hset("userId:" + userCount, {"userName": username, "score": score}, redis.print);
+    });
+    // client.hkeys("userId:" + userCount, function (err, replies) {
+    //     console.log(replies.length + " replies:");
+    //     replies.forEach(function (reply, i) {
+    //         console.log("    " + i + ": " + reply);
+    //     });
+    // });
+    userCount++;
+    client.set("userCount", userCount, redis.print);
+    res.send('Create user Success ' + username + " ; " + score);
+    
 });
 //Get User
 conUserHttp.get('/user/:userId', (req, res) =>
