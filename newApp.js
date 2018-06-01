0db1ef597f47d0bac62fcc5d653def63aab7ee49
Truong Nguyen <nhattruong159@gmail.com>
2018-06-01 14:21:34 +0700

    + update database

--- newApp.js
+++ newApp.js
@@ -45,53 +45,86 @@ conAdmin.listen(config.admin.port,()=>{
 
 /*Update Content*/
 //Add User
-
+const userKey = 'userId:';
 // parse various different custom JSON types as JSON
 conUserHttp.use(jsonParser);
 conUserHttp.put('/user', (req, res) =>
-{
-    
+{    
     var username = req.body.username;
     var score = req.body.score;
-    
-    console.log("userCount= " + userCount);
-
+    var numUpdate = req.body.numUpdate;    
     client.get("userCount", function(err, obj)
     {
-        client.hset("userId:" + userCount,"userName", username, redis.print);
-        client.hset("userId:" + userCount,"score", score, redis.print);
-
-        //client.hset("userId:" + userCount, {"userName": username, "score": score}, redis.print);
+        client.hset(userKey + userCount,"userName", username, redis.print);
+        client.hset(userKey + userCount,"score", score, redis.print);
+        client.hset(userKey + userCount,"numUpdate", numUpdate, redis.print);
+        userCount++;
+        client.set("userCount", userCount, redis.print);
+        res.send('Success ' + JSON.stringify(obj));
     });
-    // client.hkeys("userId:" + userCount, function (err, replies) {
-    //     console.log(replies.length + " replies:");
-    //     replies.forEach(function (reply, i) {
-    //         console.log("    " + i + ": " + reply);
-    //     });
-    // });
-    userCount++;
-    client.set("userCount", userCount, redis.print);
-    res.send('Create user Success ' + username + " ; " + score);
-    
 });
 //Get User
 conUserHttp.get('/user/:userId', (req, res) =>
 {
-    res.send('Get user info Success');
+    var userId = req.params.userId;
+    client.hgetall(userKey + userId, function(err, obj)
+    {
+        res.send('Success'  + JSON.stringify(obj));
+    });
 });
 
 //Post User
 conUserHttp.post('/user/:userId', (req, res) =>
 {
-    var id = req.params.userId;
-    sendWsMsg(id);
-    res.send('Update user success');
+    var userId = req.params.userId;
+    client.hgetall(userKey+userId, function(err, obj)
+    {
+        if(obj != null && err == null)
+        {
+            var userNameNew = req.body.username;
+            var scoreNew = req.body.score;
+
+            var numUpdate = obj.numUpdate;
+            numUpdate++;
+            obj.numUpdate = numUpdate;
+            var newObj = JSON.stringify(obj);
+
+            client.hset(userKey + userId,"userName", userNameNew, redis.print);
+            client.hset(userKey + userId,"score", scoreNew, redis.print);
+            client.hset(userKey + userId,"numUpdate", numUpdate, redis.print);
+            
+            var args = [ 'leaderboard', scoreNew, userId];
+            client.zadd(args, function (err, response) 
+            {
+                console.log(err);    
+            });
+            sendWsMsg(newObj);
+            res.send(newObj);        
+        }
+        else
+        {
+            res.send(404);        
+        }
+    });
 });
 
 //Delete Use
 conAdmin.delete('/user/:userId', (req, res) =>
 {
-    res.send('Delege user success');
+    var userId = req.params.userId;
+    var hashKey = userKey + userId;
+    client.hgetall(hashKey, function(err, obj)
+    {
+        if(obj != null && err == null)
+        {
+            client.del(hashKey);
+            res.send(200);
+        }
+        else
+        {
+            res.send(404);
+        }
+    });
 });
 
 function sendWsMsg(content)
